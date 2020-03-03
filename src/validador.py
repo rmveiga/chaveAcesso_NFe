e8e3fa622de7fc7c9c50d0b9497e2f284bed2b12
Rafael Morán <rm.veiga26@gmail.com>
2020-03-03 16:27:52 -0300

    Validação de CNPJ

--- src/validador.py
+++ src/validador.py
@@ -1,4 +1,5 @@
 from datetime import date
+import re
 
 ano_atual = date.today().year
 mes_atual = date.today().month
@@ -75,6 +76,51 @@ def valida_ano_mes(ano_mes):
     return v
 
 
+def valida_cnpj(cnpj):
+    #  Verifica se o tipo do parâmetro é string
+    if not isinstance(cnpj, str):
+        return False
+
+    #  Remove caracteres indesejáveis
+    cnpj_ = re.sub("[^0-9]", '', cnpj)
+
+    #  Verifica se o CNPJ possui 14 caracteres
+    if len(cnpj_) != 14:
+        return False
+
+    sum = 0
+    weight = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+
+    """ Calcula o primeiro número do dígito verificador """
+    for n in range(12):
+        value = int(cnpj_[n]) * weight[n]
+        sum = sum + value
+
+    verifyingDigit = sum % 11
+
+    if verifyingDigit < 2:
+        firstVerifyingDigit = 0
+    else:
+        firstVerifyingDigit = 11 - verifyingDigit
+
+    """ Calcula o segundo número do dígito verificador """
+    sum = 0
+    weight = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+    for n in range(13):
+        sum = sum + int(cnpj_[n]) * weight[n]
+
+    verifyingDigit = sum % 11
+
+    if verifyingDigit < 2:
+        secondVerifyingDigit = 0
+    else:
+        secondVerifyingDigit = 11 - verifyingDigit
+
+    if cnpj_[-2:] == "%s%s" % (firstVerifyingDigit, secondVerifyingDigit):
+        return True
+    return False
+
+
 def valida_chave_acesso(chave_acesso):
     valido = True
     if not tamanho_chave_acesso(chave_acesso):
@@ -89,5 +135,8 @@ def valida_chave_acesso(chave_acesso):
     if not valida_ano_mes(chave_acesso[2:6]):
         valido = False
         erros.append('Data de emissão inválida')
+    if not valida_cnpj(chave_acesso[6:20]):
+        valido = False
+        erros.append('CNPJ inválido')
 
     return valido, erros
