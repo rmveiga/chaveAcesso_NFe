3c898f279bd1cb174d7791e8162b0ef652a175a6
Rafael Morán <rm.veiga26@gmail.com>
2020-03-05 10:13:30 -0300

    Implementado loop para validar outras chaves de acesso
    principal.py
    - Alterado para fazer um loop infinito de leitura de novas chaves e perguntar ao usuário se deseja continuar ou não
    
    util.py
    - Incluido no projeto o arquivo util.py

--- src/principal.py
+++ src/principal.py
@@ -1,18 +1,29 @@
 from src.validador import valida_chave_acesso
+from src.util import *
 
 print('*' * 50)
 print(f' {"VALIDAÇÃO DE CHAVE DE ACESSO NF-e":^50} ')
 print('*' * 50)
 
-chave_acesso = str(input('Informe a chave de acesso: ')).strip()
-resultado, lista_erros = valida_chave_acesso(chave_acesso)
+while True:
+    chave_acesso = str(input('Informe a chave de acesso: ')).strip()
+    resultado, lista_erros = valida_chave_acesso(chave_acesso)
 
-if resultado:
-    print('CHAVE DE ACESSO VÁLIDA')
-else:
-    print('CHAVE DE ACESSO INÁLIDA')
-    print('*' * 50)
-    print(f' {"LISTAGEM DE ERROS":^50} ')
-    print('*' * 50)
-    for erro in lista_erros:
-        print(erro)
+    if resultado:
+        print('CHAVE DE ACESSO VÁLIDA')
+    else:
+        print('CHAVE DE ACESSO INÁLIDA')
+        print('*' * 50)
+        print(f' {"LISTAGEM DE ERROS":^50} ')
+        print('*' * 50)
+        for erro in lista_erros:
+            print(erro)
+    print('-' * 50)
+
+    opcao = str(input('Deseja validar outra chave de acesso? [S/N] ')).strip().upper()[0]
+    while opcao not in 'SN':
+        msg_erro('Opção inválida')
+        opcao = str(input('Deseja validar outra chave de acesso? [S/N] ')).strip().upper()[0]
+
+    if opcao == 'N':
+        break
---
+++ src/util.py
@@ -0,0 +1,2 @@
+def msg_erro(msg):
+    return print(f'\033[31m{msg}\033[m')
